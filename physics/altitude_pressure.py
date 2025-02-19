30d2f9887367e1d79c4df1bb86a11aa2b553e6ce
subhaniin <70527643+subhaniin@users.noreply.github.com>
2025-02-19 16:48:54 +0530

    Update altitude_pressure.py

--- physics/altitude_pressure.py
+++ physics/altitude_pressure.py
@@ -44,6 +44,7 @@ def get_altitude_at_pressure(pressure: float) -> float:
     if pressure < 0:
         raise ValueError("Atmospheric Pressure can not be negative !")
     return 44_330 * (1 - (pressure / 101_325) ** (1 / 5.5255))
+    #The correct exponent in the barometric formula is 1/5.255, but you mistakenly wrote 1/5.5255
 
 
 if __name__ == "__main__":
