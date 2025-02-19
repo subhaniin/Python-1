975b6f8e77e77cde3d28596315c5d7cb61147a8a
subhaniin <70527643+subhaniin@users.noreply.github.com>
2025-02-19 16:55:48 +0530

    Update altitude_pressure.py
    Corrected exponent: Changed 1/5.5255 to 1/5.255, aligning with the standard barometric formula.

--- physics/altitude_pressure.py
+++ physics/altitude_pressure.py
@@ -43,7 +43,7 @@ def get_altitude_at_pressure(pressure: float) -> float:
         raise ValueError("Value Higher than Pressure at Sea Level !")
     if pressure < 0:
         raise ValueError("Atmospheric Pressure can not be negative !")
-    return 44_330 * (1 - (pressure / 101_325) ** (1 / 5.5255))
+    return 44_330 * (1 - (pressure / 101_325) ** (1 / 5.255))
 
 
 if __name__ == "__main__":
